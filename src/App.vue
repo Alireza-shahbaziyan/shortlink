10dbbd481660ccb2462f8f889daf8496e93d6899
Alireza shahbaziyan <87496598+Alireza-shahbaziyan@users.noreply.github.com>
2023-02-02 00:46:13 -0800

    router added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <script>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import Header from './components/Header.vue';
+import Footer from './views/Footer.vue';
 export default{
   components:{
-    Header
+    Header,Footer
   }
 }
 </script>
 
+
 <template>
-  <div class="bg-slate-800">
+  <div class="bg-slate-800 h-screen flex flex-col justify-between">
     <Header/>
     <RouterView/>
+    <Footer/>
   </div>
 </template>
 
--- src/components/DownloadCard.vue
+++ src/components/DownloadCard.vue
@@ -1,11 +1,10 @@
 <template>
     <div>
-        <div class="card w-96 bg-base-100 shadow-xl">
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does ?</p>
+        <div class="card md:w-96 w-78 bg-base-100 shadow-xl">
+            <div class="card-body items-center text-center md:p-16 p-6">
+                <div class="font-semibold m-2 md:m-0">Click to Download</div>
                 <div class="card-actions">
-                    <button class="btn btn-primary">Download</button>
+                    <button class="btn btn-success">Download</button>
                 </div>
             </div>
         </div>
